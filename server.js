a3b25c0e763331fde6ccd0f1383eb53d59a228a7
Brandon Jernigan <brandon@whatisboom.com>
2015-02-15 20:19:34 -0600

    added posts endpoint

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var morgan = require('morgan');
+
 var rawjs = require('raw.js');
 var reddit = new rawjs('reddit-gallery/0.0.1 by /u/whatisboom');
 
@@ -20,13 +22,50 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.json());
 
-router.get('/oauth', function(req, res) {
+router.use(morgan('tiny'));
+
+router.route('/oauth')
+.get(function(req, res) {
 
     reddit.setupOAuth2(config.reddit.clientId, config.reddit.secret, config.reddit.redirectUri);
 
-    var url = reddit.authUrl('dev', ['identity']);
+    var url = reddit.authUrl('dev', ['identity'], true);
 
     res.json({ url: url });
+})
+.post(function(req, res) {
+    var code = req.body.code;
+
+    reddit.auth({ code: code }, function(err, auth) {
+        if (err) {
+            cosole.log('auth failure: ' + err);
+            res.status(500).json({err: err});
+        }
+        else {
+            reddit.refreshToken = auth.refreshToken;
+            reddit.me(function(err, user) {
+                res.json({
+                    auth: auth,
+                    user: user
+                });
+            });
+        }
+    });
+});
+
+router.route('/posts/:subreddit')
+.get(function(req, res) {
+    reddit.hot({
+        r: req.params.subreddit,
+        limit: 100
+    }, function(err, response) {
+        if (err) {
+            res.status(500).json({err: err});
+        }
+        else {
+            res.json(response);
+        }
+    })
 });
 
 app.use(router);
