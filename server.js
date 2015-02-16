ee3fe635ddecfc495537b2c5e7cf0213e3066afa
Brandon Jernigan <brandon@whatisboom.com>
2015-02-15 18:27:21 -0600

    added express, and basic oauth endpoint

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 var config = require('./app/config');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
 var rawjs = require('raw.js');
 var reddit = new rawjs('reddit-gallery/0.0.1 by /u/whatisboom');
-reddit.setupOAuth2(config.reddit.clientId, config.reddit.secret, config.reddit.redirectUri);
 
-var url = reddit.authUrl('dev', ['identity']);
 
-console.log(url);
+var port = 8080;
+
+var router = express.Router();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(bodyParser.json());
+
+router.get('/oauth', function(req, res) {
+
+    reddit.setupOAuth2(config.reddit.clientId, config.reddit.secret, config.reddit.redirectUri);
+
+    var url = reddit.authUrl('dev', ['identity']);
+
+    res.json({ url: url });
+});
+
+app.use(router);
+app.listen(port);
+console.log('Running on port: ' + port);
