c7cc1122f1340906a3d3656d85198d2c4834bce5
Brandon Jernigan <brandon@whatisboom.com>
2015-08-14 21:38:46 -0500

    file formatting

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ var port = 8080;
 var router = express.Router();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.use(bodyParser.json());
@@ -27,45 +27,45 @@ router.use(morgan('tiny'));
 router.route('/oauth')
 .get(function(req, res) {
 
-    reddit.setupOAuth2(config.reddit.clientId, config.reddit.secret, config.reddit.redirectUri);
+  reddit.setupOAuth2(config.reddit.clientId, config.reddit.secret, config.reddit.redirectUri);
 
-    var url = reddit.authUrl('dev', ['identity'], true);
+  var url = reddit.authUrl('dev', ['identity'], true);
 
-    res.json({ url: url });
+  res.json({ url: url });
 })
 .post(function(req, res) {
-    var code = req.body.code;
+  var code = req.body.code;
 
-    reddit.auth({ code: code }, function(err, auth) {
-        if (err) {
-            cosole.log('auth failure: ' + err);
-            res.status(500).json({err: err});
-        }
-        else {
-            reddit.refreshToken = auth.refreshToken;
-            reddit.me(function(err, user) {
-                res.json({
-                    auth: auth,
-                    user: user
-                });
-            });
-        }
-    });
+  reddit.auth({ code: code }, function(err, auth) {
+    if (err) {
+      cosole.log('auth failure: ' + err);
+      res.status(500).json({err: err});
+    }
+    else {
+      reddit.refreshToken = auth.refreshToken;
+      reddit.me(function(err, user) {
+        res.json({
+          auth: auth,
+          user: user
+        });
+      });
+    }
+  });
 });
 
 router.route('/posts/:subreddit')
 .get(function(req, res) {
-    reddit.hot({
-        r: req.params.subreddit,
-        limit: 100
-    }, function(err, response) {
-        if (err) {
-            res.status(500).json({err: err});
-        }
-        else {
-            res.json(response);
-        }
-    })
+  reddit.hot({
+    r: req.params.subreddit,
+    limit: 100
+  }, function(err, response) {
+    if (err) {
+      res.status(500).json({err: err});
+    }
+    else {
+      res.json(response);
+    }
+  })
 });
 
 app.use(router);
